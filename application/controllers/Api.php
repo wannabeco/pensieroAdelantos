1a99c6cb2a4d14f86b2ceed3277cfc4c9998deb5
Farez Prieto <desarrollo@wannabe.com.co>
2020-08-13 07:01:43 -0500

    Ajustes del archivo API

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -66,7 +66,6 @@ class Api extends CI_Controller
                 $mensaje    .=   "<strong>".$codigo."</strong>";
                 //plantilla del mail
                 $plantilla   = plantillaMail($asunto,$mensaje);
-                //envio el mail
                 sendMail($para,$asunto,$plantilla);
                 $salida = array("mensaje"=>"Hemos enviado el mensaje al correo electrónico seleccionado, por favor verifique.",
                                 "datos"=>array(),
