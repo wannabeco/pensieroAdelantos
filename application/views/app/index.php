001f3c26e1227a33364b05cd3f6d9aedafe2b03f
Farez Prieto <kyo20052@gmail.com>
2020-09-02 07:31:19 -0500

    Se agrega el Favicon

--- application/views/app/index.php
+++ application/views/app/index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+    <link href="<?php echo base_url()?>res/img/favicon.png?<?php echo rand(0,1000)?>" rel="shortcut icon"type=image/x-icon>
 
     <title><?php echo $titulo ?></title>
 
