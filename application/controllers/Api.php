5569c847d00f4b35d211e18d3ce890da60399db3
Farez Prieto <kyo20052@gmail.com>
2021-11-11 14:51:35 -0500

    Ajuste en el envío del código porque la app se queda pegada.

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -72,7 +72,7 @@ class Api extends CI_Controller
                 //envio el codigo de ingreso al mail del usuario
                 sendMail($para,$asunto,$plantilla);
                 //envio el código de acceso al whatsapp de la persona
-                sendWhatsappMessage("57".$dataempleado['datos'][0]['celularEmpleado'],$mensajeW);
+                //sendWhatsappMessage("57".$dataempleado['datos'][0]['celularEmpleado'],$mensajeW);
                 $salida = array("mensaje"=>"Hemos enviado el mensaje al correo electrónico seleccionado, por favor verifique.",
                                 "datos"=>array(),
                                 "continuar"=>1);
